34999f78e42da2b9185e657f5ac6738ea7bd3b3a
Chieh Wang <chiehwang1226@gmail.com>
2020-10-06 10:33:20 -0600

    take out myrecipe in the header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -31,7 +31,6 @@ const Header = ({ logoutCurrentUser, currentUserToken }) => {
       <Link className='web-logo' to='/'>Secret Recipes</Link>
       <div className='header-options'>
         <Link className='header-option' to='/explore'>Explore</Link>
-        <Link className='header-option' to='/myrecipes'>My Recipes</Link>
         <Link className='header-option' to='/signin'>
           <Button variant="dark" type="button" onClick={() => handleClick()}>
           {
