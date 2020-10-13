d932b04ff6bf39a7d4edeec448a557d5da368375
Chieh Wang <chiehwang1226@gmail.com>
2020-10-12 19:27:00 -0600

    added create new recipe form (without handling changes)

--- src/components/recipe-preview/RecipePreview.jsx
+++ src/components/recipe-preview/RecipePreview.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { withRouter } from 'react-router-dom';
 import './RecipePreview.styles.scss';
 
+import AccessTimeIcon from '@material-ui/icons/AccessTime';
+import WhatshotIcon from '@material-ui/icons/Whatshot';
+import PersonOutlineIcon from '@material-ui/icons/PersonOutline';
+
 const RecipePreview = ({ recipe, match, history }) => {
   const { _id, title, preparation, cook_time, servings } = recipe;
   return (
@@ -14,17 +18,22 @@ const RecipePreview = ({ recipe, match, history }) => {
       })}
     >
       <div className='foodimg_container'>
-        {
-          recipe.img ? <img alt='foodimg' src={`data:image/png;base64,${recipe.img}`} /> 
-          : <img alt='default_foodimg' src={require('../../utils/food_default.png')} />
-        }
+      {
+        recipe.img ? <img alt='foodimg' src={`data:image/png;base64,${recipe.img}`} /> 
+        : <img alt='default_foodimg' src={require('../../utils/food_default.png')} />
+      }
       </div>
       <h4>{title}</h4>
-      <h6>Preparation time: {preparation}</h6>
-      <h6>Cook time: {cook_time}</h6>
-      <h6>Servings: {servings}</h6>
+      <div className='recipe-preview-icons'>
+        <div className='recipe-preview-row1'>
+          <AccessTimeIcon /><span style={{paddingLeft: "5px", paddingRight: "15px", fontSize: "17px"}}>{preparation}</span>
+          <WhatshotIcon /><span style={{paddingLeft: "5px", fontSize: "17px"}}>{cook_time}</span>
+        </div>
+        <PersonOutlineIcon /><span style={{paddingLeft: "5px", fontSize: "17px"}}>{servings}</span>
+      </div>
     </div>
   );
 }
+
 
 export default withRouter(RecipePreview);
--- src/containers/create-recipe-page/CreateRecipePage.jsx
+++ src/containers/create-recipe-page/CreateRecipePage.jsx
@@ -1,11 +1,75 @@
 import React from 'react';
-import './CreateRecipePage.styles.css';
+import './CreateRecipePage.styles.scss';
+import { withRouter } from 'react-router-dom';
 
-const CreateRecipePage = () => {
+import { Button, TextField } from "@material-ui/core";
+import KeyboardBackspaceIcon from '@material-ui/icons/KeyboardBackspace';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+
+const CreateRecipePage = (props) => {
   return (
     <div className='create-recipe-page-container'>
+      <div className='create-recipe-fields'>
+        <Button onClick={() => props.history.goBack()}
+          type="button" startIcon={<KeyboardBackspaceIcon />}>
+            Go Back
+        </Button>
+        <form noValidate autoComplete="off">
+          <TextField className='recipe-title-field' id="recipe-title" fullWidth label="Recipe Title" />
+          <div className='category-field'>
+            <h6 style={{verticalAlign: "center"}}>Category:</h6>
+            <FormControlLabel control={<Checkbox name="checkedDessert" />} style={{}} label="Dessert" />
+            <FormControlLabel control={<Checkbox name="checkedMeal" />} label="Meal" />
+            <FormControlLabel control={<Checkbox name="checkedDrink" />} label="Drink" />
+          </div>
+          <div className='time-field'>
+            <h6>Prep time: </h6>
+            <TextField id="standard-number" label="Number" type="number" style={{margin: "0px 30px"}} />
+            <h6>minutes</h6>
+          </div>
+          <div className='time-field'>
+            <h6>Cook time:</h6>
+            <TextField id="standard-number" label="Number" type="number" style={{margin: "0px 30px"}}/> 
+            <h6>minutes</h6>
+          </div>
+          <div className='time-field'>
+            <h6>Number of Servings:</h6>
+            <TextField id="standard-number" label="Number" type="number" style={{margin: "0px 30px"}}/> 
+            <h6>poeple</h6>
+          </div>
+
+          <div className='ingredients-steps-field'>
+            <h6>Ingredients: </h6>
+            <TextField
+              id="outlined-multiline-static"
+              label="Separate the ingredients with enter key"
+              multiline
+              fullWidth
+              rows={8}
+              placeholder="2 ripe avocados&#10;1/4 teaspoon of salt&#10;1/4 cup chopped onion"
+              variant="outlined"
+            />
+          </div>
+          <div className='ingredients-steps-field'>
+            <h6>Steps: </h6>
+            <TextField
+              id="outlined-multiline-static"
+              label="Separate your steps with enter key"
+              multiline
+              fullWidth
+              rows={8}
+              placeholder="Bring a large pot of lightly salted water to a boil.&#10;Preheat oven to 425 degrees F (220 degrees C).&#10;Bake for 15 to 20 minutes in the preheated oven."
+              variant="outlined"
+            />
+          </div>
+          <div className='create-recipe-submitbtn'>
+            <Button variant="outlined" type="submit" size="large">Submit</Button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
 
-export default CreateRecipePage;
+export default withRouter(CreateRecipePage);
