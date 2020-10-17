d862b2d634db2a2728b805daf11eb58d3aa7e981
Chieh Wang <chiehwang1226@gmail.com>
2020-10-17 00:15:28 -0600

    changed how to render component

--- src/containers/App.js
+++ src/containers/App.js
@@ -34,12 +34,12 @@ function App({ currentUserToken }) {
           </div>
             <Switch>
               <Route exact path='/' component={HomePage} />
-              <Route exact path='/explore' component={()=><ExplorePage />} />
+              <Route exact path='/explore' component={ExplorePage} />
               <Route exact path='/myrecipes' render={() => currentUserToken ? (<UserRecipePage />) : (<Redirect to='/signin' />)} />
               <Route exact path='/signin' render={() => currentUserToken ? (<Redirect to='/myrecipes' />) : (<SignInSignUpPage />)} />
-              <Route exact path='/createrecipe' component={()=><CreateRecipePage />} />
-              <Route exact path='/updaterecipe' component={()=><UpdateRecipePage />} />
-              <Route exact path='/editrecipephoto' component={()=><EditRecipePhotoPage />} />
+              <Route exact path='/createrecipe' component={CreateRecipePage} />
+              <Route exact path='/updaterecipe' component={UpdateRecipePage} />
+              <Route exact path='/editrecipephoto' component={EditRecipePhotoPage} />
               <Route path='/explore/:id' component={RecipePage} />
               <Route path='/myrecipes/:id' component={RecipePage} />
             </Switch>
