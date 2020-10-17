41e1dfa53f22a7eea5545fc2569ea802daa89d26
Chieh Wang <chiehwang1226@gmail.com>
2020-10-17 00:53:43 -0600

    added thunkMiddleware into store

--- src/redux/store.js
+++ src/redux/store.js
@@ -6,8 +6,10 @@ import logger from 'redux-logger';
 import rootReducer from './root-reducer';
 
 const middlewares = [];
+middlewares.push(thunkMiddleware);
+
 if (process.env.NODE_ENV === 'development') {
-  middlewares.push(logger, thunkMiddleware);
+  middlewares.push(logger);
 }
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 export const persistor = persistStore(store);
