6fbfdbf6d54233500abf4ae3ded0f90fef1ae204
Chieh Wang <chiehwang1226@gmail.com>
2020-10-28 16:20:52 -0600

    footer changed

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,9 +7,9 @@ const Footer = () => {
       <p>© Copyright 2020. Created by{' '}
         <a target="_blank" rel="noopener noreferrer"
            href="https://stephanieportfolio-39f22.firebaseapp.com/#home">
-             Stephanie Wang and Fangyu Hsu.
+             Stephanie Wang
         </a>{' '}
-        Designed by Fangyu Hsu.
+        and Fangyu Hsu. Designed by Fangyu Hsu.
       </p>
     </div>
   );
