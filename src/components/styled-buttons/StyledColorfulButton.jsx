0bb4a3eaff88c9d3072fef856b0719cffdd53e74
Chieh Wang <chiehwang1226@gmail.com>
2020-10-27 21:44:52 -0600

    style

--- src/components/styled-buttons/StyledColorfulButton.jsx
+++ src/components/styled-buttons/StyledColorfulButton.jsx
@@ -5,7 +5,7 @@ const StyledColorfulButton = withStyles({
   root: {
     background: 'linear-gradient(45deg, #FFA497 30%, #FED259 90%)',
     borderRadius: 20,
-    color: '#4F4F4F',
+    color: '#ffffff',
     height: "33px",
     width: "90px",
     fontSize: "15px"
