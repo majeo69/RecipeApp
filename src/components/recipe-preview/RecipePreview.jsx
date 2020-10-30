ebbc13bde9d51dfe99f7a19b37caf6c71f72dcf7
Chieh Wang <chiehwang1226@gmail.com>
2020-10-30 14:30:56 -0600

    added recipe's owner's name on preview page

--- src/components/recipe-preview/RecipePreview.jsx
+++ src/components/recipe-preview/RecipePreview.jsx
@@ -6,7 +6,7 @@ import TimerIcon from '@material-ui/icons/Timer';
 import PersonOutlineIcon from '@material-ui/icons/PersonOutline';
 
 const RecipePreview = ({ recipe, match, history }) => {
-  const { _id, title, preparation, cook_time, servings } = recipe;
+  const { _id, title, preparation, cook_time, servings, owner_name } = recipe;
   return (
     <div className='recipe-container' 
       onClick={() => history.push({
@@ -22,7 +22,10 @@ const RecipePreview = ({ recipe, match, history }) => {
         : <img alt='default_foodimg' src={require('../../assets/food_default.png')} />
       }
       </div>
-      <h4>{title}</h4>
+      <div className='preview-title-name'>
+        <h4>{title}</h4>
+        <h6>by {owner_name}</h6>
+      </div>
       <div className='recipe-preview-icons'>
         <div className='recipe-preview-row1'>
           <TimerIcon /><span style={{paddingLeft: "5px", paddingRight: "15px", fontSize: "20px"}}><span style={{fontSize: "17px"}}>Prep: </span>{preparation} <span style={{fontSize: "16px"}}>mins</span></span>
